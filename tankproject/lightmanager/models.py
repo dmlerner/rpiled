7c573a0f6c154e87b2faafe85c05581ef292c6a2
David Lerner <dmlerner@gmail.com>
2021-12-12 11:12:50 -0800

    needs rpi stuff

--- tankproject/lightmanager/models.py
+++ tankproject/lightmanager/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+def bound_duty(d):
+    return max(0, min(2**16-1, d))
+def to_duty_cycle(percent):
+    return bound_duty(int(float(percent)/100*(2**16-1)))
+def to_percent(duty_cycle):
+    return float(duty_cycle) / (2**16-1)*100
+
 def myround(x):
     if x < .1:
         return round(x, 2)
@@ -15,17 +22,21 @@ class PCA9685(models.Model):
 class PWMChannel(models.Model):
     controller = models.ForeignKey(PCA9685, on_delete=models.CASCADE)
     index = models.IntegerField(unique=True)
-    duty_cycle = models.IntegerField()
+    duty_cycle = models.IntegerField(default=0)
     color = models.CharField()
-    driver_current = models.IntegerField()
+    driver_current = models.IntegerField(default=700) # mA
     color_abbreviation = models.CharField()
 
     def get_percent(self):
         return self.duty_cycle # TODO
 
-    def set_percent(self, p):
-        duty_cycle = PWMChannel.to_duty_cycle(p)
-            self.duty_cycle = joi
+    def set_percent(self, p, pca):
+        self.duty_cycle = to_duty_cycle(p)
+        pca.channels[self.index].duty_cycle = self.duty_cycle
 
     def __str__(self):
         return ' | '.join(map(str, (self.color, round(self.get_percent()))))
+
+    '''
+    '''
+
--- tankproject/lightmanager/views.py
+++ tankproject/lightmanager/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
+from . import models
+import board
+import busio
+import adafruit_pca9685
+
+pca = None
+
+def get_pca():
+    global pca
+    if pca:
+        return pca
+
+    pca_model = models.PCA9685.get_or_create()
+    i2c_bus = busio.I2C(busio.SCL, busio.SDA)
+    pca = adafruit_pca9685.PCA9685(i2c_bus)
+    return pca
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def set_brightness(request, channel_id, brightness_percent):
+    pca = get_pca()
+    channel = PWMChannel.get_or_create(channel_id)
+    channel.set_percent(brightness_percent, pca)
+    print(channel_id, brightness_percent)
+    return HttpResponse(f'Setting channel {channel_id} to {brightness_percent}%')
