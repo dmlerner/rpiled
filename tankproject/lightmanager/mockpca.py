a8a73f6307e9f1710c1d9b30ee0121fd9e95ad98
David Lerner <dmlerner@gmail.com>
2022-12-27 14:31:13 -0800

    organizing pca, mock, real

--- tankproject/lightmanager/mockpca.py
+++ tankproject/lightmanager/mockpca.py
@@ -1,1 +1,23 @@
-asdf
+from dataclasses import dataclass
+import pca
+
+N_CHANNELS = 8
+
+@dataclass
+class MockChannel:
+    duty_cycle: int = pca.MAX_DUTY_CYCLE
+
+class MockPCA9685:
+    def __init__(self, n=N_CHANNELS, frequency=pca.FREQUENCY):
+        self.channels = [MockChannel() for i in range(n)]
+        self.frequency = frequency
+
+class MockPCA(pca.AbstractPCA):
+    def __init__(self):
+        self.hardware_pca = MockPCA9685()
+
+    def set_brightness(self, channel, milli_percent, relative=False, scale=False, pca=None):
+        pass
+
+    def set_brightnesses(self, milli_percents, relative=False, scale=False):
+        pass
--- tankproject/lightmanager/pca.py
+++ tankproject/lightmanager/pca.py
@@ -2,8 +2,22 @@ import time
 import traceback
 import utils
 
-imported = False
 DEBUG = False
+MAX_DUTY_CYCLE = 2**16-1
+FREQUENCY = 2441
+
+class AbstractPCA:
+    def __init__(self):
+        self.hardware_pca = None
+
+    def get_pca(self):
+        return self.hardware_pca
+
+    def set_brightness(self, channel, milli_percent, relative=False, scale=False, pca=None):
+        pass
+
+    def set_brightnesses(self, milli_percents, relative=False, scale=False):
+        pass
 
 def debug(*x):
     if DEBUG:
@@ -18,46 +32,6 @@ def get_elapsed_time(show=True):
     return delta
 get_elapsed_time.t = time.time()
 
-def import_all():
-    global imported, board, busio, adafruit_pca9685
-    if imported:
-        return True
-
-    try:
-        import board
-        import busio
-        import adafruit_pca9685
-        imported = True
-    except Exception:
-        traceback.debug_exc()
-
-    return imported
-
-#pca = None
-MAX_DUTY_CYCLE = 2**16-1
-PCA = None
-
-def get_pca(frequency=None):
-    global PCA
-    if PCA:
-        debug('already initialized pca, returning', PCA)
-        return PCA
-
-    #global pca
-    #if not pca:
-    if not import_all():
-        debug('failed to get pca')
-        return
-    i2c_bus = busio.I2C(board.SCL, board.SDA)
-    PCA = adafruit_pca9685.PCA9685(i2c_bus)
-    PCA.frequency = 2441
-
-    #if frequency is not None and pca.frequency != frequency:
-        #debug('setting freq', frequency)
-        #pca.frequency = frequency
-    debug('made new pca, returning: ', PCA)
-    return PCA
-
 def bound_duty(d):
     return max(0, min(MAX_DUTY_CYCLE, d))
 
@@ -70,39 +44,3 @@ def to_duty_cycle(milli_percent):
 
 def to_milli_percent(duty_cycle):
     return float(duty_cycle) / (2**16-1)*100*MILLI
-
-def set_brightness(channel, milli_percent, relative=False, scale=False, pca=None):
-    duty_cycle = to_duty_cycle(milli_percent)
-    debug('chan, mp, dc, relative, scale', channel, milli_percent, duty_cycle, relative, scale)
-
-    pca = pca or get_pca()
-    if not pca:
-        return
-
-    duty_cycle_before = pca.channels[channel].duty_cycle
-    assert not (relative and scale)
-    if relative:
-        duty_cycle += duty_cycle_before
-    if scale:
-        duty_cycle = duty_cycle_before * milli_percent
-
-    duty_cycle = bound_duty(int(duty_cycle))
-    if utils.close(duty_cycle, duty_cycle_before):
-        return
-
-    pca.channels[channel].duty_cycle = duty_cycle
-    #after = [c.duty_cycle for c in pca.channels]
-    #debug(before)
-    #debug(after)
-    get_elapsed_time()
-    #assert before != after
-
-def set_brightnesses(milli_percents, relative=False, scale=False):
-    debug('set_brightnesses', milli_percents)
-    pca = get_pca()
-    if not pca:
-        debug('not pca')
-        return
-
-    for cid, mp in milli_percents.items():
-        set_brightness(cid, mp, relative, scale, pca)
