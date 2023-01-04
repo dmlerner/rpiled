844b29fcf7a4ec07d8c61530b1ce83595f2b8737
David Lerner <dmlerner@gmail.com>
2023-01-04 08:39:39 -0800

    log level

--- tankproject/tankproject/settings.py
+++ tankproject/tankproject/settings.py
@@ -138,7 +138,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['file'],
-            'level': 'DEBUG',
+            'level': 'INFO',
             'propagate': True,
         },
     },
