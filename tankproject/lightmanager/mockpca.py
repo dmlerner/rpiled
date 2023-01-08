e4529198ca09d34318077d589a721cc63759b329
David Lerner <dmlerner@gmail.com>
2023-01-08 10:06:35 -0800

    .

--- tankproject/lightmanager/mockpca.py
+++ tankproject/lightmanager/mockpca.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from lightmanager import pca
 from lightmanager import utils
+# TODO: either rename this "local server pca" or merger with test mock pca
 
 N_CHANNELS = 8
 
@@ -26,82 +27,3 @@ class MockPCA(pca.BasePCA):
     def __init__(self, models):
         self._pca = MockPCA9685()
         self.models = models
-
-
-MILLI = 100  # TODO: change this to 1000?
-PERCENT = 100
-MILLI_PERCENT = MILLI * PERCENT
-
-
-def verify(actual, expected, close=True):
-    if close:
-        if all(utils.close_rel(a, e) for (a, e) in zip(actual, expected)):
-            return
-    elif actual == expected:
-        return
-    print(actual, expected)
-    assert False
-
-
-def test_absolute_zero():
-    p = MockPCA()
-    expected_duty_cycles = [0] * N_CHANNELS
-    verify(p.get_duty_cycles(), expected_duty_cycles)
-
-
-def test_absolute_too_low():
-    p = MockPCA()
-    expected_duty_cycles = [0] * N_CHANNELS
-    p.set_brightness(0, -10, False, False)
-    verify(p.get_duty_cycles(), expected_duty_cycles)
-
-
-def test_absolute_max():
-    p = MockPCA()
-    expected_duty_cycles = [0] * N_CHANNELS
-    p.set_brightness(0, pca.MAX_DUTY_CYCLE, False, False)
-    expected_duty_cycles[0] = pca.MAX_DUTY_CYCLE
-    verify(p.get_duty_cycles(), expected_duty_cycles)
-
-
-def test_absolute_too_high():
-    p = MockPCA()
-    expected_duty_cycles = [0] * N_CHANNELS
-    p.set_brightness(0, pca.MAX_DUTY_CYCLE * 2, False, False)
-    expected_duty_cycles[0] = pca.MAX_DUTY_CYCLE
-    verify(p.get_duty_cycles(), expected_duty_cycles)
-
-
-def test_absolute_happy_path():
-    p = MockPCA()
-    expected_duty_cycles = [0] * N_CHANNELS
-
-    raw_proportion = 0.5
-    brightness = raw_proportion * MILLI_PERCENT
-    p.set_brightness(0, brightness, False, False)
-    expected_duty_cycles[0] = pca.MAX_DUTY_CYCLE * raw_proportion
-    verify(p.get_duty_cycles(), expected_duty_cycles)
-
-
-def test_relative():
-    pass
-
-
-def test_scale():
-    pass
-
-
-def test():
-    for k, v in globals().items():
-        if k.startswith("test_"):
-            print(k)
-            try:
-                v()
-                print("passes!")
-            except:
-                print("fails" + "!" * 20)
-            print()
-
-
-if __name__ == "__main__":
-    test()
